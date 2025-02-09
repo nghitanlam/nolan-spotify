9545523ab1af6118b278debbba9b739311f5b328
Lam Tan Nghi <nghi.tanlam@gmail.com>
2025-02-09 22:27:14 +0700

    BE_ [USER] post callback route added

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,26 @@
+import { User } from "../models/user.model";
+
+export const authCallback = async (req, res) => {
+  try {
+    const { id, firstName, lastName, imageUrl } = req.body;
+
+    // [1] Check if user already exists
+    const user = await User.findOne({ clerkId: id });
+
+    if (!user) {
+      //[2] signup
+      await User.create({
+        clerkId: id,
+        fullName: `${firstName} ${lastName}`,
+        imageUrl,
+      });
+    }
+
+    // [-1] return data
+    res.status(200).json({ success: true, message: "Success" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: `Internal Server Error ${error}` });
+  }
+};
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,7 +1,9 @@
 import express from "express";
 
+import { authCallback } from "../controllers/auth.controller";
+
 const router = express.Router();
 
-router.get("/", () => {});
+router.post("/callback", authCallback);
 
 export default router;
