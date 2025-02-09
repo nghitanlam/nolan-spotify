248ea31bc91cf7e23f981c2e6ece044c13c12c55
Lam Tan Nghi <nghi.tanlam@gmail.com>
2025-02-09 20:44:10 +0700

    FE_ frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button variant={"outline"}>This is a button</Button>
+    </>
+  );
+}
+
+export default App;
