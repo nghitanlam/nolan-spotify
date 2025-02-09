0be4b9fbe80a3c29fb09924c007c5bddac773c71
Lam Tan Nghi <nghi.tanlam@gmail.com>
2025-02-09 22:57:15 +0700

    BE_ file upload middleware added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { clerkMiddleware } from "@clerk/express";
+import fileUpload from "express-fileupload";
 
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
@@ -13,12 +15,23 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT;
 
 app.use(express.json());
 
 // [1] call this middleware then when user loggedin, the id of user will store in req.user.userId
 app.use(clerkMiddleware());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024, // Max 10MB
+    },
+  }),
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
