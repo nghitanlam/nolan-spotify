2fa3d0c3fc6c3e71e0dfef2785f93faf932993fe
Lam Tan Nghi <nghi.tanlam@gmail.com>
2025-02-10 21:05:38 +0700

    BE_ update auth route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,9 @@
 import express from "express";
 
+import { authCallback } from "../controllers/auth.controller.js";
+
 const router = express.Router();
 
-router.get("/", () => {});
+router.get("/callback", authCallback);
 
 export default router;
