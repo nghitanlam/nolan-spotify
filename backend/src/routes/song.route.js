1feb7572471fcacee59ca3ae71c96ac5e969c719
Lam Tan Nghi <nghi.tanlam@gmail.com>
2025-02-10 21:04:03 +0700

    BE_ [SONG] get all songs, get featured songs, get made for you songs, get trending songs added

--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -1,7 +1,18 @@
 import express from "express";
 
+import {
+  getAllSongs,
+  getFeaturedSongs,
+  getMadeForYouSongs,
+  getTrendingSongs,
+} from "../controllers/song.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+
 const router = express.Router();
 
-router.get("/", () => {});
+router.get("/", protectRoute, requireAdmin, getAllSongs);
+router.get("/featured", getFeaturedSongs);
+router.get("/made-for-you", getMadeForYouSongs);
+router.get("/trending", getTrendingSongs);
 
 export default router;
