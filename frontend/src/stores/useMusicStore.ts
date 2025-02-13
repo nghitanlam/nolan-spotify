cfd361d47ea9b468e8d446ec9f8ca9131383859a
Lam Tan Nghi <nghi.tanlam@gmail.com>
2025-02-13 21:35:18 +0700

    FE_ zustand and left sidebar added

---
+++ frontend/src/stores/useMusicStore.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+
+import { axiosInstance } from "@/lib/axios";
+import { Album, Song } from "@/types";
+
+interface MusicStore {
+  songs: Song[];
+  albums: Album[];
+  isLoading: boolean;
+  error: string | null;
+
+  fetchAlbums: () => Promise<void>;
+}
+
+export const useMusicStore = create<MusicStore>((set) => ({
+  albums: [],
+  songs: [],
+  isLoading: false,
+  error: null,
+
+  fetchAlbums: async () => {
+    set({ isLoading: true, error: null });
+
+    try {
+      const response = await axiosInstance.get("/albums");
+      set({ albums: response.data.data });
+    } catch (error: any) {
+      set({ error: error.response.data.message });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+}));
