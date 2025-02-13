d3ea6b0b451a96d9e3070d4ff533a703f5d60fdf
Lam Tan Nghi <nghi.tanlam@gmail.com>
2025-02-13 18:44:03 +0700

    FE_ main layout added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from "react-router-dom";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 
+import MainLayout from "./layout/MainLayout";
 import HomePage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
-import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import ChatPage from "./pages/chat/ChatPage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route
           path="/sso-callback"
           element={
@@ -18,6 +19,11 @@ function App() {
           }
         />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Route>
       </Routes>
     </>
   );
---
+++ frontend/src/layout/MainLayout.tsx
@@ -0,0 +1,51 @@
+import { Outlet } from "react-router-dom";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import LeftSidebar from "./components/LeftSidebar";
+
+const MainLayout = () => {
+  const isMobile = false;
+
+  return (
+    <div className="h-screen bg-black text-white flex flex-col">
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="flex-1 flex h-full overflow-hidden p-2"
+      >
+        {/* Left */}
+        <ResizablePanel
+          defaultSize={20}
+          minSize={isMobile ? 0 : 10}
+          maxSize={30}
+        >
+          <LeftSidebar />
+        </ResizablePanel>
+
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+
+        {/* Main content */}
+        <ResizablePanel defaultSize={isMobile ? 80 : 60}>
+          <Outlet />
+        </ResizablePanel>
+
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+
+        {/* Right */}
+        <ResizablePanel
+          defaultSize={20}
+          minSize={0}
+          maxSize={25}
+          collapsedSize={0}
+        >
+          friends component
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
+};
+
+export default MainLayout;
